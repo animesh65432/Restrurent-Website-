697dbd7308f800e33d0cee3d26ccce2895a6115d
animesh65432 <kiranduttta234@gmail.com>
2024-02-08 13:26:58 +0530

    Add Cart buttom

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 import classes from "./Header.module.css";
 import { HeaderImages } from "../../Images/Images";
+import HeaderCartbuttom from "./HeaderCartbuttom";
 
 const Header = (props) => {
   return (
     <React.Fragment>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <button>Cart</button>
+        <HeaderCartbuttom />
       </header>
       <div className={classes["main-image"]}>
         <img src={HeaderImages} alt="Headerimages"></img>
---
+++ src/Components/Layout/HeaderCartbuttom.js
@@ -0,0 +1,17 @@
+import React from "react";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartbuttom.module.css";
+const HeaderCartbuttom = (props) => {
+  return (
+    <>
+      <button className={classes.button}>
+        <span className={classes.icon}>
+          <CartIcon />
+        </span>
+        <span>Your Cart</span>
+        <span className={classes.badge}>3</span>
+      </button>
+    </>
+  );
+};
+export default HeaderCartbuttom;
